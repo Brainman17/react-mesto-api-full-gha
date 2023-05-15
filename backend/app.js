f0479c5e69d7d338df47935363ff646e276d483c
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-05-15 18:53:29 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const cors = require('cors');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -17,14 +17,17 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(cors());
+
 app.use(express.json());
+
 app.use(requestLogger); // подключаем логгер запросов
 
 // за ним идут все обработчики роутов
 app.post('/signin', celebrates.signIn, login);
 app.post('/signup', celebrates.signUp, createUser);
 
-app.use(auth);
+//app.use(auth);
 
 app.use(userRouter);
 app.use(cardRouter);
@@ -43,10 +46,6 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Application listening on ${PORT}!`);
 });
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
 
 // cloud-jegor-andreichuk - облако
 
