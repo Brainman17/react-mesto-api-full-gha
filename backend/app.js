1a25e0261f2d125df48d3b48c7f8d595ae133e12
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-05-15 17:15:24 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const app = express();
 const mongoose = require('mongoose');
