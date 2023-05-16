68f9b63491d8613912923602c9c2b6858afcd11d
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-05-16 14:00:36 +0300

    fix

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -85,7 +85,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-59",
+  baseUrl: "https://jegor-andreychuk.students.nomoredomains.monster",
   headers: {
     authorization: "35c121f9-a929-4389-abb0-8fb8cd7ce78b",
     "Content-Type": "application/json",
