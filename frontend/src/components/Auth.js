b3f055da8af02b7227d6f85634f0abf05f20e16c
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-06-11 21:54:16 +0300

    fix

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:3005";
+export const BASE_URL = "https://jegor-andreychuk.yandex.nomoredomains.monster";
 // "https://jegor-andreychuk.yandex.nomoredomains.monster";
 // "http://127.0.0.1:3005"
 
