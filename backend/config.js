4a1686e88e03f617e67d0133d2d85e5aa449eb2b
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-06-12 16:41:59 +0300

    fix: исправить переменные окружения

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 
 const { NODE_ENV } = process.env;
 const { PORT = 3000 } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const key = process.env.JWT_SECRET || 'my-dev-key';
+
+console.log(process.env.NODE_ENV);
+console.log(process.env.JWT_SECRET);
 
 module.exports = {
   key,
